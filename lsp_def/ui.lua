5d476e59d5c64b1e30b0fff42c89a5b27a4c8ed5
makatymba2001 <makatymba2001@yandex.ru>
2025-11-01 23:18:13 +0200

    Definitely not gay commit

--- lsp_def/ui.lua
+++ lsp_def/ui.lua
@@ -1,16 +1,16 @@
 --- @meta
 
 --- UIBox definition and config for Domain or Scenario in blind choices list
----@param index number
----@param total number
----@param scenario? TheEncounter.ScenarioResolvable
----@param domain TheEncounter.DomainResolvable
----@return table
+--- @param index number
+--- @param total number
+--- @param scenario? TheEncounter.ScenarioResolvable
+--- @param domain TheEncounter.DomainResolvable
+--- @return table
 function TheEncounter.UI.event_choice_render(index, total, scenario, domain) end
 
 --- UIBox definition for Domain or Scenario in blind choices list
----@param choices { scenario_key?: string, domain_key: string }[]
----@return table
+--- @param choices { scenario_key?: string, domain_key: string }[]
+--- @return table
 function TheEncounter.UI.event_choices_render(choices) end
 
 --- Move event choices in visible area
