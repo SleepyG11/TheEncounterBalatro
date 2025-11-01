897fb3a50eb1c03b7ea872b44d3bf9603bf366fe
makatymba2001 <makatymba2001@yandex.ru>
2025-11-01 15:13:13 +0200

    Small order adjustments

--- src/logic/blind_select.lua
+++ src/logic/blind_select.lua
@@ -13,9 +13,11 @@ function Game:update_enc_event_select(dt)
 	if not G.STATE_COMPLETE then
 		stop_use()
 		ease_background_colour_blind(G.STATES.BLIND_SELECT)
-		if not G.GAME.TheEncounter_choices then
-			G.GAME.TheEncounter_choices = TheEncounter.poll_choices()
-		end
+		G.GAME.TheEncounter_choices_amount = G.GAME.TheEncounter_choices_amount or 2
+		G.GAME.TheEncounter_choices_args = G.GAME.TheEncounter_choices_args or {
+			increment_usage = true,
+		}
+		G.GAME.TheEncounter_choices = G.GAME.TheEncounter_choices or TheEncounter.poll_choices()
 		G.E_MANAGER:add_event(Event({
 			func = function()
 				save_run()
@@ -32,9 +34,9 @@ function Game:update_enc_event_select(dt)
 						play_sound("cancel")
 						G.TheEncounter_blind_choices =
 							UIBox(TheEncounter.UI.event_choices_render(G.GAME.TheEncounter_choices))
-						TheEncounter.UI.set_event_choices()
-
 						G.TheEncounter_prompt_box = UIBox(TheEncounter.UI.prompt_box_render())
+
+						TheEncounter.UI.set_event_choices()
 						TheEncounter.UI.set_prompt_box()
 						G.CONTROLLER.lock_input = false
 						return true
@@ -61,11 +63,6 @@ function CardArea:draw(...)
 end
 
 TheEncounter.poll_choices = function()
-	G.GAME.TheEncounter_choices_amount = G.GAME.TheEncounter_choices_amount or 2
-	G.GAME.TheEncounter_choices_args = G.GAME.TheEncounter_choices_args or {
-		increment_usage = true,
-	}
-
 	local duplicates_list = {}
 	for _, item in ipairs(G.GAME.TheEncounter_choices or {}) do
 		if item.scenario_key then
